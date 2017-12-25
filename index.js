e82192169b0f6ad92c1e070d3a1f691e638bb93d
Ashton Reed <dreamergirl411@gmail.com>
2017-12-25 20:05:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatles = {};
-  for (i=0; i<musicians.length; i++) {
+  for (let i=0; i<musicians.length; i++) {
     beatles.push(`${musicians} plays ${instruments}`)
   }
-  return beatles 
+  return beatles
 }
